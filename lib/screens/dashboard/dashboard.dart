0c4d08f05f06c62163e8df1914f07c71c7552e41
Filip Novosel <filip.novosel@gmail.com>
2022-12-22 20:26:00 +0100

    fix(Dashboard): Restore logo to its previous location

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:rocllection/data/stone/stone_model.dart';
 import 'package:rocllection/data/stone/stone_provider.dart';
 import 'package:rocllection/screens/dashboard/widgets/stone_list_tile.dart';
+import 'package:rocllection/utils/globals.dart';
 import 'package:rocllection/widgets/logo.dart';
 
 class Dashboard extends StatefulWidget {
@@ -22,28 +23,42 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     final stoneProvider = Provider.of<StoneProvider>(context);
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
       body: RefreshIndicator(
         onRefresh: () async => stoneProvider.loadAllStones(),
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: stoneProvider.stones.length,
-          itemBuilder: (context, index) {
-            final Stone stone = stoneProvider.stones[index];
-            print(index);
-            return Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-              child: StoneListTile(
-                stone: Stone(
-                    addedDate: stone.addedDate,
-                    description: stone.description,
-                    id: stone.id,
-                    imageUrl: stone.imageUrl,
-                    name: stone.name,
-                    color: stone.color),
-              ),
-            );
-          },
+        child: ListView(
+          children: [
+            const Center(
+              child: Logo(),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              itemCount: stoneProvider.stones.length,
+              itemBuilder: (context, index) {
+                final Stone stone = stoneProvider.stones[index];
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+                  child: StoneListTile(
+                    stone: Stone(
+                        addedDate: stone.addedDate,
+                        description: stone.description,
+                        id: stone.id,
+                        imageUrl: stone.imageUrl,
+                        name: stone.name,
+                        color: stone.color),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: Globals.spacingBelowFormButton,
+            )
+          ],
         ),
       ),
     );
